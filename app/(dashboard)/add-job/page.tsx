8435f0c19f65bd204d5343f1759354cc80559477
Amandeep Singh <amandeepsingh@emerson.com>
2024-06-28 14:54:25 +0530

    Internal pages

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddJobPage = () => {
+  return <div>add page</div>;
+};
+
+export default AddJobPage;
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DashboardLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => children;
+
+export default DashboardLayout;
