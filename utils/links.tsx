3ec5d9c2b8383899aac36bec5d04ac9ee44254cc
Amandeep Singh <amandeepsingh@emerson.com>
2024-06-28 16:43:38 +0530

    Nav links

---
+++ utils/links.tsx
@@ -0,0 +1,28 @@
+import { AppWindow, AreaChart, Layers } from "lucide-react";
+import React from "react";
+
+type NavLink = {
+  href: string;
+  label: string;
+  icon: React.ReactNode;
+};
+
+const links: NavLink[] = [
+  {
+    href: "/add-job",
+    label: "add job",
+    icon: <Layers />,
+  },
+  {
+    href: "/jobs",
+    label: "jobs",
+    icon: <AppWindow />,
+  },
+  {
+    href: "/stats",
+    label: "stats",
+    icon: <AreaChart />,
+  },
+];
+
+export default links;
