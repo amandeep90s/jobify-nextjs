792cf69baef07fdf71ae93b21028c1795b9b13e9
Amandeep Singh <programmeramandeep@gmail.com>
2024-06-30 16:57:46 +0530

    Jobs page setup

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import JobsList from "@/components/JobsList";
+import SearchForm from "@/components/SearchForm";
+import { getAllJobsAction } from "@/utils/actions";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 
-const JobsPage = () => {
+const JobsPage = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["jobs", "", "all", 1],
+    queryFn: () => getAllJobsAction({}),
+  });
+
   return (
-    <div>page</div>
-  )
-}
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SearchForm />
+      <JobsList />
+    </HydrationBoundary>
+  );
+};
 
-export default JobsPage
+export default JobsPage;
