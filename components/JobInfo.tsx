80571eacc18b713c66bc1d5bc65c09b0465c05c3
Amandeep Singh <programmeramandeep@gmail.com>
2024-06-30 20:27:25 +0530

    Add css class for job mode

--- components/JobInfo.tsx
+++ components/JobInfo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const JobInfo = ({ icon, text }: { icon: React.ReactNode; text: string }) => {
   return (
     <div className="flex gap-x-2 items-center">
-      {icon} {text}
+      {icon} <span className="capitalize">{text.replaceAll("-", " ")}</span>
     </div>
   );
 };
