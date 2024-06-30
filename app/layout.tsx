311aec6aedb65e5aa40798274ab2ba25320fcbca
Amandeep Singh <programmeramandeep@gmail.com>
2024-06-30 11:49:57 +0530

    Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Jobify App",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning={true}>
         <body>
           <Providers>{children}</Providers>
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
