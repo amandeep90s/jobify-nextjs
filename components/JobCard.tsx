26a821846e4a4bbd4d9b9d78f688f84ce69de0c5
Amandeep Singh <programmeramandeep@gmail.com>
2024-06-30 19:27:08 +0530

    Job card component

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,7 +1,36 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { JobType } from "@/utils/types";
+import Link from "next/link";
+import DeleteJobButton from "./DeleteJobButton";
 
 const JobCard = ({ job }: { job: JobType }) => {
-  return <div>JobCard</div>;
+  return (
+    <Card className="bg-muted">
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent></CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild size={"sm"}>
+          <Link className="capitalize" href={`/jobs/${job.id}`}>
+            edit
+          </Link>
+        </Button>
+        <DeleteJobButton />
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default JobCard;
