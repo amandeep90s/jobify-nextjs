9a7c8060717c2d0e06475582dbec5aab19d5baa9
Amandeep Singh <programmeramandeep@gmail.com>
2024-06-30 13:58:09 +0530

    Get all jobs action

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,11 +1,10 @@
 "use server";
 
-import prisma from "./db";
 import { auth } from "@clerk/nextjs/server";
-import { JobType, CreateAndEditJobType, createAndEditJobSchema } from "./types";
-import { redirect } from "next/navigation";
 import { Prisma } from "@prisma/client";
-import dayjs from "dayjs";
+import { redirect } from "next/navigation";
+import prisma from "./db";
+import { CreateAndEditJobType, JobType, createAndEditJobSchema } from "./types";
 
 const authenticateAndRedirect = (): string => {
   const { userId } = auth();
@@ -25,5 +24,60 @@ export const createJobAction = async (
   } catch (error) {
     console.log(error);
     return null;
+  }
+};
+
+type GetAllJobsActionTypes = {
+  search?: string;
+  jobStatus?: string;
+  page?: number;
+  limit?: number;
+};
+
+export const getAllJobsAction = async ({
+  search,
+  jobStatus,
+  page = 1,
+  limit = 10,
+}: GetAllJobsActionTypes): Promise<{
+  jobs: JobType[];
+  count: number;
+  page: number;
+  totalPages: number;
+}> => {
+  const userId = authenticateAndRedirect();
+
+  try {
+    let whereClause: Prisma.JobWhereInput = {
+      clerkId: userId,
+    };
+
+    if (search) {
+      whereClause = {
+        ...whereClause,
+        OR: [
+          {
+            position: { contains: search },
+          },
+          {
+            company: { contains: search },
+          },
+        ],
+      };
+    }
+
+    if (jobStatus && jobStatus !== "all") {
+      whereClause = { ...whereClause, status: jobStatus };
+    }
+
+    const jobs: JobType[] = await prisma.job.findMany({
+      where: whereClause,
+      orderBy: { createdAt: "desc" },
+    });
+
+    return { jobs, count: 0, page: 1, totalPages: 0 };
+  } catch (error) {
+    console.log(error);
+    return { jobs: [], count: 0, page: 1, totalPages: 0 };
   }
 };
