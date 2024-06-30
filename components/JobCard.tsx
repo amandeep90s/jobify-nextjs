6d7e8930594b648c1374c825ebe1285c6ff79ce4
Amandeep Singh <programmeramandeep@gmail.com>
2024-06-30 19:33:31 +0530

    JobInfo Component

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -9,10 +9,15 @@ import {
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { JobType } from "@/utils/types";
+import { Briefcase, CalendarDays, MapPin, RadioTower } from "lucide-react";
 import Link from "next/link";
 import DeleteJobButton from "./DeleteJobButton";
+import JobInfo from "./JobInfo";
+import { Badge } from "./ui/badge";
 
 const JobCard = ({ job }: { job: JobType }) => {
+  const date = new Date(job.createdAt).toLocaleDateString();
+
   return (
     <Card className="bg-muted">
       <CardHeader>
@@ -20,7 +25,17 @@ const JobCard = ({ job }: { job: JobType }) => {
         <CardDescription>{job.company}</CardDescription>
       </CardHeader>
       <Separator />
-      <CardContent></CardContent>
+      <CardContent className="mt-4 grid grid-cols-2 gap-4">
+        <JobInfo icon={<Briefcase />} text={job.mode} />
+        <JobInfo icon={<MapPin />} text={job.location} />
+        <JobInfo icon={<CalendarDays />} text={date} />
+        <Badge className="w-32 justify-center">
+          <JobInfo
+            icon={<RadioTower className="w-4 h-4" />}
+            text={job.status}
+          />
+        </Badge>
+      </CardContent>
       <CardFooter className="flex gap-4">
         <Button asChild size={"sm"}>
           <Link className="capitalize" href={`/jobs/${job.id}`}>
