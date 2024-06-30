cd4fb1baafbc0d1f14cff78184df67e65314279d
Amandeep Singh <programmeramandeep@gmail.com>
2024-06-30 17:11:58 +0530

    SearchForm component setup

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,5 +1,46 @@
+"use client";
+
+import { JobStatus } from "@/utils/types";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const search = formData.get("search") as string;
+    const jobStatus = formData.get("jobStatus") as string;
+    console.log(search, jobStatus);
+  };
+
+  return (
+    <form
+      className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
+      onSubmit={handleSubmit}
+    >
+      <Input type="text" placeholder="Search Jobs" name="search" />
+      <Select name="jobStatus">
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {["all", ...Object.values(JobStatus)].map((jobStatus) => (
+            <SelectItem key={jobStatus} value={jobStatus}>
+              {jobStatus}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Button type="submit">Search</Button>
+    </form>
+  );
 };
 
 export default SearchForm;
