3af31bbcba51ca5a7e8f3972c37f12bb0e88fba4
Amandeep Singh <programmeramandeep@gmail.com>
2024-07-01 20:45:07 +0530

    Fixed issue in edit job form page

--- app/(dashboard)/jobs/[id]/page.tsx
+++ app/(dashboard)/jobs/[id]/page.tsx
@@ -3,12 +3,11 @@ import { getSingleJobAction } from "@/utils/actions";
 import {
   dehydrate,
   HydrationBoundary,
-  useQueryClient,
+  QueryClient,
 } from "@tanstack/react-query";
-import React from "react";
 
 const JobDetailPage = async ({ params }: { params: { id: string } }) => {
-  const queryClient = useQueryClient();
+  const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ["job", params.id],
