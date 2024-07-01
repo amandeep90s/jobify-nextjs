7953ee4afd7e0b0f92157fc6dc46489a84733393
Amandeep Singh <programmeramandeep@gmail.com>
2024-07-01 21:54:47 +0530

    Loading Skeleton

--- app/(dashboard)/jobs/loading.tsx
+++ app/(dashboard)/jobs/loading.tsx
@@ -1,5 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 const loading = () => {
-  return <div>loading</div>;
+  return (
+    <div className="p-8 grid sm:grid-cols-2 gap-4 md:grid-cols-3 rounded-lg">
+      <Skeleton className="h-10" />
+      <Skeleton className="h-10" />
+      <Skeleton className="h-10" />
+    </div>
+  );
 };
 
 export default loading;
--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -1,4 +1,5 @@
 import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Skeleton } from "./ui/skeleton";
 
 type StatsCardProps = {
   title: string;
@@ -13,6 +14,22 @@ const StatsCard = ({ title, value }: StatsCardProps) => {
         <CardDescription className="text-4xl font-extrabold text-primary mt-[0px!important]">
           {value}
         </CardDescription>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export const StatsLoadingCard = () => {
+  return (
+    <Card className="w-[330px] h-[88px]">
+      <CardHeader className="flex flex-row justify-between items-center">
+        <div className="flex items-center space-x-4">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[150px]" />
+            <Skeleton className="h-4 w-[100px]" />
+          </div>
+        </div>
       </CardHeader>
     </Card>
   );
