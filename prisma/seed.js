867442f7c9b4250271a1822101587234e891801f
Amandeep Singh <amandeepsingh@emerson.com>
2024-07-01 14:43:56 +0530

    Seed Database

---
+++ prisma/seed.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require("@prisma/client");
+const data = require("./mock-data.json");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const clerkId = "user_id";
+  const jobs = data.map((job) => ({ ...job, clerkId }));
+
+  for (const job of jobs) {
+    await prisma.job.create({ data: job });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async () => {
+    await prisma.$disconnect();
+  });
